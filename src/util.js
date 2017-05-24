0daefbd0d7684c432fcaae1c47e9509304582ccc
David Castelblanco <cbdavides@gmail.com>
2017-05-24 12:15:05 -0500

    Se agrega función auxiliar para generar la cadena rgb

--- src/util.js
+++ src/util.js
@@ -3,3 +3,9 @@ let util = {};
 util.mod = function(x,  N){
     return (x % N + N) % N;
 }
+
+util.rgb = function(i) {
+    let g = Math.floor(255 - 42.5 * i) % 255;
+    let b = Math.floor(255 - 42.5 * 3) % 255;
+    return `rgb(0, ${g}, ${b})`;
+}
