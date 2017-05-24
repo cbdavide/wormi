6f7b4bd9f73106c41add882e1b65319cb77cf89e
David Castelblanco <cbdavides@gmail.com>
2017-05-24 08:29:28 -0500

    Se cambia la forma en la que se empaqueta el juego

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 'use strict';
 
-(function(canvas){
+function Game(canvas){
+    console.log(window);
+    this.ctx = canvas.getContext('2d');
+    window.addEventListener('keydown', move.bind(this), false);
 
-    let ctx = canvas.getContext('2d');
-
-    let b = new Ball(0, 50, 'blue');
-    b.paint(ctx, {x: 75, y: 75});
-    let c = new Ball(1, 35, 'blue');
-    c.paint(ctx, {x: 75, y: 75});
-    let d = new Ball(2, 25, 'blue');
-    d.paint(ctx, {x: 75, y: 75});
+    this.wormi = new Worm();
+    let b = new Ball(0, 25, 'blue');
+    let c = new Ball(1, 25, 'green');
+    this.wormi.add(b);
+    this.wormi.paint(this.ctx, {x: 25, y: 25});
     // ctx.clearRect(0, 0, 600, 60);
+    this.wormi.add(c);
+    // wormi.paint(ctx, {x: 50, y: 0});
 
+    // let c = new Ball(1, 35, 'blue');
+    // c.paint(ctx, {x: 75, y: 75});
+    // let d = new Ball(2, 25, 'blue');
+    // d.paint(ctx, {x: 75, y: 75});
 
-})(document.getElementById('canvas'));
+};
+
+new Game(document.getElementById('canvas'));
