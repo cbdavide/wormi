73bafdf063db35ac981e550e362a32b614eb8fc4
David Castelblanco <cbdavides@gmail.com>
2017-05-24 12:16:53 -0500

    Se agregan los eventos hardcodeados.

--- src/events.js
+++ src/events.js
@@ -2,25 +2,13 @@
 
 function move(e) {
 
-    if(e.keyCode == 37) { // Left
-        this.wormi.paint(this.ctx, {
-            x: conf.rate_of_change,
-            y: 0
-        });
+    if(e.keyCode == 37) { // Right
+        this.wormi.move(-30, 0);
     } else if(e.keyCode == 38) { // Up
-        this.wormi.paint(this.ctx, {
-            x: 0,
-            y: -conf.rate_of_change
-        });
-    } else if(e.keyCode == 39) { // Right
-        this.wormi.paint(this.ctx, {
-            x: -conf.rate_of_change,
-            y: 0
-        });
+        this.wormi.move(0, -30);
+    } else if(e.keyCode == 39) { // Left
+        this.wormi.move(30, 0);
     } else if (e.keyCode == 40) { // Down
-        this.wormi.paint(this.ctx, {
-            x: 0,
-            y: conf.rate_of_change
-        });
+        this.wormi.move(0, 30);
     }
 }
